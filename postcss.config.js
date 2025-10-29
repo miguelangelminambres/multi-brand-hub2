fc058a1554132445b5f9da2f28ceca02d9f53168
xianmihu <miguelangelminambres@sekou.es>
2025-10-29 19:06:10 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -4,18 +4,3 @@ module.exports = {
     autoprefixer: {},
   },
 }
-```
-4. **Commit**
-
-### **3.5 - Crear .gitignore**
-1. **"Add file" → "Create new file"**
-2. Nombre: `.gitignore`
-3. Contenido:
-```
-node_modules
-/.next/
-/out/
-.DS_Store
-*.pem
-.env*.local
-.vercel
